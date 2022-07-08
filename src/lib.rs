59efa5e489de104fb9534239375b771981ae52bc
nawat <n.ngerncham@gmail.com>
2022-07-08 16:31:15 +0700

    git outer implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 mod dot_prod;
 use dot_prod::dot;
 
+mod outer_prod;
+use outer_prod::outer;
+
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -14,5 +17,6 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 fn numpar(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(dot, m)?)?;
+    m.add_function(wrap_pyfunction!(outer, m)?)?;
     Ok(())
 }
